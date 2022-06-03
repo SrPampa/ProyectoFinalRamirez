a95c1e73a6328cc7ad97947069cd4c3bac6d1f6a
SrPampa <raulpampa2@gmail.com>
2022-06-03 19:57:51 +0200

    Creacion de proyecto vacío

---
+++ src/main/java/dam/venancio/ramirez/proyectoFinal/App.java
@@ -0,0 +1,13 @@
+package dam.venancio.ramirez.proyectoFinal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
