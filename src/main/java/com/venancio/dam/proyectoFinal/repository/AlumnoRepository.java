5fd615aa311a7500392067a12ca6205d37615b47
Ruth.H <ruth.herqui@educa.jcyl.es>
2022-06-05 13:09:16 +0200

    alumno repository actualizado

--- src/main/java/com/venancio/dam/proyectoFinal/repository/AlumnoRepository.java
+++ src/main/java/com/venancio/dam/proyectoFinal/repository/AlumnoRepository.java
@@ -1,5 +1,42 @@
 package com.venancio.dam.proyectoFinal.repository;
 
+import java.util.Set;
+
+import com.venancio.dam.proyectoFinal.dao.AlumnoDAO;
+import com.venancio.dam.proyectoFinal.model.Alumno;
+
 public class AlumnoRepository {
 
+	private AlumnoDAO dao;
+	
+	private static AlumnoRepository instance;
+	
+	public static synchronized AlumnoRepository getInstance() {
+		if (instance ==null) {
+			instance = new AlumnoRepository ();
+		}
+		return instance;
+	}
+
+	public AlumnoRepository() {
+		this.dao = new AlumnoDAO();
+	}
+
+	public Set<Alumno> getAlumnos() {
+		return this.dao.getAlumnos();
+	}
+
+	public boolean addAlumno(Alumno a) {
+		// TODO Auto-generated method stub
+		return this.dao.addAlumno(a);
+	}
+
+	public int updateAlumno(int cod, Alumno a) {
+		return dao.modificarAlumno(a, cod);
+	}
+
+	public boolean deleteAlumno(int cod) {
+		return dao.deleteAlumno(cod);
+	
+	}
 }
